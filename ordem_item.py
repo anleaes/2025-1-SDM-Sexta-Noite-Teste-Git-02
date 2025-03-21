5d65619acd99d35cf04a1e7c34d3aaceee333319
iraMlemmaH <anleaes@gmail.com>
2025-03-21 20:17:24 -0300

    alteraçao em ordem_item

---
+++ ordem_item.py
@@ -0,0 +1,10 @@
+class OrderItem:
+    def __init__(self, quantidade, preco_unitario):
+        self.quantidade = quantidade
+        self.preco_unitario = preco_unitario
+        self.date_fabrication = date_fabrication
+        self.is_active = is_active
+        self.category = category
+        
+
+        
